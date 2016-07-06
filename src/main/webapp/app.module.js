8fc1df9016bde804b7dbe40faabbdc7f9618ad17
Shylo Ihor <ISylo@nvision-group.com>
2016-07-06 19:05:22 +0300

    BS-1 Configured jasmine plugin on Maven + created unitTest and testData

--- src/main/webapp/app.module.js
+++ src/main/webapp/app.module.js
@@ -1,1 +1,16 @@
-angular.module('bookStoreApp', []);
+angular.module('bookStoreApp', [])
+    .controller('BookListController', function ($scope) {
+        $scope.books = [
+            {
+                name: 'War and Peace',
+                snippet: 'Very big book.'
+            }, {
+                name: '1984',
+                snippet: 'Very smart book'
+            }, {
+                name: 'Sherlock Holms',
+                snippet: 'Just a classic detective.'
+            }
+        ];
+    })
+;
