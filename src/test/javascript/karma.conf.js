ec733f1f3ee85f9b80a9a1f92282bff074b8e627
Casak123 <casak123@gmail.com>
2016-07-07 18:54:46 +0300

    BS-3 Configure E2E tests in our project

---
+++ src/test/javascript/karma.conf.js
@@ -0,0 +1,26 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '../../..',
+        frameworks: ['jasmine'],
+        files: [
+            'src/main/webapp/lib/angular/angular.min.js',
+            'src/main/webapp/lib/angular-route/angular-route.min.js',
+            'src/main/webapp/lib/angular-resource/angular-resource.min.js',
+            'src/main/webapp/lib/angular-animate/angular-animate.min.js',
+            'src/main/webapp/lib/angular-mocks/angular-mocks.js',
+            'src/main/webapp/*.js',
+            'src/test/javascript/*.js'
+        ],
+        exclude: ['src/test/javascript/karma.conf*.js'],
+        reporters: ['progress'],
+        port: 9876,
+        logLevel: config.LOG_INFO,
+        browsers: ['Chrome'],
+        singleRun: false,
+        autoWatch: true,
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher'
+        ]
+    });
+};
