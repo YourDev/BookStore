8220b28c3598bc71c3535653e6dd1e064f896f1f
Casak123 <casak123@gmail.com>
2016-07-13 16:08:59 +0300

    Revert to 5f1d436

--- src/main/webapp/app.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('bookStoreApp')
-    .directive('header', function (){
-        return {
-            templateUrl: "templates/header.template.html"};
-    })
-    .directive('footer', function (){
-        return {
-            templateUrl: "templates/footer.template.html"};
-    });
