efd41f050f766089c48a104c749e80ab85ab8be9
Shylo Ihor <ISylo@nvision-group.com>
2016-07-19 18:00:01 +0300

    BS-13 Created directives in app.conf + created templates for for footer and header

--- src/main/webapp/app.config.js
+++ src/main/webapp/app.config.js
@@ -1,15 +1,28 @@
 angular.module('bookStoreApp').config([
-    '$locationProvider', '$routeProvider',
-    function config($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
+        '$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
+            $locationProvider.hashPrefix('!');
 
-        $routeProvider.
-        when('/books', {
-            template: '<book-list></book-list>'
-        }).
-        when('/books/:bookId', {
+            $routeProvider.when('/books', {
+                template: '<book-list></book-list>'
+            }).when('/books/:bookId', {
                 template: '<book-detail></book-detail>'
-            }).
-        otherwise('/books');
-    }
-]);
+            }).otherwise('/books');
+        }
+    ])
+
+    .directive("footer", function () {
+        return {
+            restrict: "A",
+            replace: true,
+            templateUrl: "footer/footer.html"
+        }
+    })
+
+    .directive("header", function () {
+        return {
+            restrict: "A",
+            replace: true,
+            templateUrl: "header/header.html"
+        }
+    });
