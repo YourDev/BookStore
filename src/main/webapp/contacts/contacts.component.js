bed0f30a619deaa2fa5ee4936ea044ba858c3890
Shylo Ihor <ISylo@nvision-group.com>
2016-07-26 17:33:24 +0300

    BS-20 Some options changed for final view of the map

--- src/main/webapp/contacts/contacts.component.js
+++ src/main/webapp/contacts/contacts.component.js
@@ -5,11 +5,25 @@ angular.module('contacts').component('contacts', {
         uiGmapGoogleMapApi.then(function (maps) {
             self.map = {
                 center: {
+                    latitude: 50.486198,
+                    longitude: 30.420599
+                },
+                options: {
+                    mapTypeId: google.maps.MapTypeId.HYBRID
+                },
+                zoom: 14
+            };
+            self.marker = {
+                id: 0,
+                coords: {
                     latitude: 50.490812,
                     longitude: 30.412617
                 },
-                zoom: 13
-            };
+                options: {
+                    draggable: false,
+                    title: 'Yourbook \nulitsa Severo-Syretskaya, 1, Kyiv'
+                }
+            }
         });
     }
     ]
