21347eca65f8380f2def6a9491c720e42f230c2d
Casak123 <casak123@gmail.com>
2016-07-26 14:52:53 +0300

    BS-21 Create view and content for Contacts page

--- src/main/webapp/contacts/contacts.component.js
+++ src/main/webapp/contacts/contacts.component.js
@@ -1,7 +1,42 @@
 angular.module('contacts').component('contacts', {
    templateUrl: 'contacts/contacts.template.html',
-   controller: [function contactsController(){
+   controller: ['$scope', function contactsController($scope){
+       var styleArray = [
+           {
+               featureType: "all",
+               stylers: [
+                   { saturation: -80 }
+               ]
+           },{
+               featureType: "road.arterial",
+               elementType: "geometry",
+               stylers: [
+                   { hue: "#00ffee" },
+                   { saturation: 50 }
+               ]
+           },{
+               featureType: "poi.business",
+               elementType: "labels",
+               stylers: [
+                   { visibility: "off" }
+               ]
+           }
+       ];
 
+       var ourPosition = {lat: 50.490802, lng:  30.412595};
+
+       var map = new google.maps.Map(document.getElementById("googleMap"),{
+           center: ourPosition,
+           scrollwheel: false,
+           styles: styleArray,
+           zoom: 10
+       });
+
+       new google.maps.Marker({
+           map: map,
+           position: ourPosition,
+           title: 'Main office!'
+       });
     }
     ]
 });
