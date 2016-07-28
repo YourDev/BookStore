98d4fbeea97b8dfae965b433d48a2889e78d2f2f
Casak123 <casak123@gmail.com>
2016-07-28 14:34:38 +0300

    BS-25 Add deletion in wishlist

--- src/main/webapp/wishList/wishList.component.js
+++ src/main/webapp/wishList/wishList.component.js
@@ -2,6 +2,12 @@ angular.module('wishList').component('wishList', {
     templateUrl: 'wishList/wishList.template.html',
     controller: ['$scope', function wishListController($scope){
         if($scope.$root.wishlist != undefined) $scope.ngIfFlag = true;
+
+        $scope.deleteBook = function(id){
+            var index = $scope.$root.wishlist
+                .indexOf($scope.$root.wishlist.find(function(book){return id === book.id}));
+            $scope.$root.wishlist.splice(index, 1);
+        };
     }
     ]
 });
