1b604c72c7863b0fd23dd42478128f5d3e576cd7
Casak123 <casak123@gmail.com>
2016-08-12 13:35:29 +0300

    Add 'add to cart' link with icon and logic. Refactor wish list variables.

--- src/main/webapp/book-detail/book-detail.controller.js
+++ src/main/webapp/book-detail/book-detail.controller.js
@@ -1,24 +1,34 @@
-angular.module('bookDetail').controller('bookDetailController', function bookDetailController($scope, $routeParams, dataService) {
+angular.module('bookDetail')
+    .controller('bookDetailController', function bookDetailController($scope, $routeParams, dataService, cartService, wishListService) {
     if($scope.$root.wishlist === undefined) $scope.$root.wishlist = [];
 
     $scope.setImage = function(imageUrl){
         $scope.mainImageUrl = imageUrl;
     };
 
-    $scope.checkWishlist = function(){
-        $scope.ngIfFlag = $scope.$root.wishlist.find(function (book) {
+    $scope.checkFlags = function(){
+        $scope.wishListFlag = wishListService.getBooks().find(function (book) {
+                return $scope.book.id === book.id;
+            }) === undefined;
+
+        $scope.cartFlag = cartService.getBooks().find(function (book) {
                 return $scope.book.id === book.id;
             }) === undefined;
     };
 
     $scope.addBookToWishList = function(){
-        $scope.$root.wishlist.push($scope.book);
-        $scope.checkWishlist();
+        wishListService.addBook($scope.book);
+        $scope.checkFlags();
+    };
+
+    $scope.addBookToCart = function(){
+        cartService.addBook($scope.book);
+        $scope.checkFlags();
     };
 
     dataService.getBook($routeParams.bookId).then(function(book){
         $scope.book = book;
         $scope.setImage($scope.book.images[0]);
-        $scope.checkWishlist();
+        $scope.checkFlags();
     });
 });
